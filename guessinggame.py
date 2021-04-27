6737a184710ce9c99daebed48769248586655e75
rachit013 <rachit200013@gmail.com>
2021-04-27 12:47:22 +0530

    Create guessinggame.py

---
+++ guessinggame.py
@@ -0,0 +1,25 @@
+import random
+
+highest = 1000
+answer = random.randint(1, highest)
+
+print(answer)
+guess = 0
+print("please guess a number between 1 to {}: ".format(highest))
+
+while guess != answer:
+    guess = int(input())
+    if guess == 0:
+        print("Thank you, please come next time")
+        break
+    if guess == answer:
+        print("well done you guessed it")
+        break
+    else:
+        if guess < answer:
+            print("please guess higher")
+        else:
+            print("please guess lower")
+
+    
+    
